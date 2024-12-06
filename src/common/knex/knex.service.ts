9918437a2bceccda207f03507eab39aa6ce19434
nickhung <nick@microsys.com.tw>
2024-12-06 15:41:08 +0800

    feat: fix路徑問題

--- src/common/knex/knex.service.ts
+++ src/common/knex/knex.service.ts
@@ -5,14 +5,14 @@ https://docs.nestjs.com/providers#services
 import { Injectable } from "@nestjs/common"
 import knex from "knex"
 import * as fs from "fs"
-
+import * as path from "path"
 @Injectable()
 export class KnexService {
   private knexInstance
 
   constructor() {
     const env = process.env.NODE_ENV || "development"
-    const configFilePath = `config.${env}.json`
+    const configFilePath = path.join(__dirname, `config.${env}.json`)
     const config = JSON.parse(fs.readFileSync(configFilePath, "utf8"))
     // 根據你的資料庫配置進行修改
     this.knexInstance = knex(config.db.mysql)
